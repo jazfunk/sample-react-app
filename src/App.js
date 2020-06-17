70daec4228ad48a9c5c2f670505a52d0f929f8ee
Jeff King <jazfunk@gmail.com>
2020-06-17 09:46:38 -0400

    Imlemented  Axios, showing random dog

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import jkLogo from './images/titleGraphicWithLogoOPTIMIZED.svg'
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import jkLogo from "./images/titleGraphicWithLogoOPTIMIZED.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>
-          Testing Heroku Deployment
-        </h2>        
-      </header>
-      <main className="App-main">
-        <img src={jkLogo} alt="jkLogo" />
-      </main>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageURL: '',
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('https://dog.ceo/api/breeds/image/random')    
+    .then(response => {
+      this.setState({ imageURL: response.data.message})      
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  render() {
+    const { imageURL } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">          
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Testing Heroku Deployment</h2>
+          <img src={imageURL} alt="Random Dog" />
+        </header>
+        <main className="App-main">
+          <img src={jkLogo} alt="jkLogo" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
